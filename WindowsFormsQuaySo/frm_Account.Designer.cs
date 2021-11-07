a39716ad76cfdd2423324d1eccce4c517537d9a5
Le Thanh Nam <namlethanh2503@gmail.com>
2021-11-07 15:38:07 +0700

    Them from Account

---
+++ WindowsFormsQuaySo/frm_Account.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace WindowsFormsQuaySo
+{
+    partial class frm_Account
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Account));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblAccount = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnReload = new System.Windows.Forms.ToolStripLabel();
+            this.btnAdd = new System.Windows.Forms.ToolStripLabel();
+            this.btnEdit = new System.Windows.Forms.ToolStripLabel();
+            this.btnDelete = new System.Windows.Forms.ToolStripLabel();
+            this.btnExit = new System.Windows.Forms.ToolStripLabel();
+            this.dgvUserList = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colHoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTenDangNhap = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colMatKhau = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRemember = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.colPhongBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblAccount.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblAccount.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccount.Location = new System.Drawing.Point(0, 0);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(973, 37);
+            this.lblAccount.TabIndex = 0;
+            this.lblAccount.Text = "Quản lý tài khoản";
+            this.lblAccount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.toolStrip1);
+            this.panel1.Controls.Add(this.dgvUserList);
+            this.panel1.Location = new System.Drawing.Point(6, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(965, 516);
+            this.panel1.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnReload,
+            this.btnAdd,
+            this.btnEdit,
+            this.btnDelete,
+            this.btnExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(965, 31);
+            this.toolStrip1.TabIndex = 16;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnReload
+            // 
+            this.btnReload.Image = global::WindowsFormsQuaySo.Properties.Resources.reload;
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(87, 28);
+            this.btnReload.Text = "Làm mới";
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Image = global::WindowsFormsQuaySo.Properties.Resources.adduser;
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(66, 28);
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Image = global::WindowsFormsQuaySo.Properties.Resources.edit;
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(54, 28);
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Image = global::WindowsFormsQuaySo.Properties.Resources.delete;
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(55, 28);
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(67, 28);
+            this.btnExit.Text = "Thoát";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dgvUserList
+            // 
+            this.dgvUserList.AllowUserToAddRows = false;
+            this.dgvUserList.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgvUserList.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvUserList.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUserList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvUserList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUserList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colHoTen,
+            this.colTenDangNhap,
+            this.colMatKhau,
+            this.colRemember,
+            this.colPhongBan});
+            this.dgvUserList.Location = new System.Drawing.Point(3, 40);
+            this.dgvUserList.Name = "dgvUserList";
+            this.dgvUserList.ReadOnly = true;
+            this.dgvUserList.RowHeadersWidth = 51;
+            this.dgvUserList.RowTemplate.Height = 32;
+            this.dgvUserList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvUserList.Size = new System.Drawing.Size(959, 473);
+            this.dgvUserList.TabIndex = 15;
+            this.dgvUserList.Click += new System.EventHandler(this.dgvUserList_Click);
+            // 
+            // colID
+            // 
+            this.colID.DataPropertyName = "ID";
+            this.colID.HeaderText = "ID";
+            this.colID.MinimumWidth = 6;
+            this.colID.Name = "colID";
+            this.colID.ReadOnly = true;
+            this.colID.Width = 50;
+            // 
+            // colHoTen
+            // 
+            this.colHoTen.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colHoTen.DataPropertyName = "hoTen";
+            this.colHoTen.HeaderText = "Tên hiển thị";
+            this.colHoTen.MinimumWidth = 6;
+            this.colHoTen.Name = "colHoTen";
+            this.colHoTen.ReadOnly = true;
+            // 
+            // colTenDangNhap
+            // 
+            this.colTenDangNhap.DataPropertyName = "tenDangNhap";
+            this.colTenDangNhap.HeaderText = "Tên đăng nhập";
+            this.colTenDangNhap.MinimumWidth = 6;
+            this.colTenDangNhap.Name = "colTenDangNhap";
+            this.colTenDangNhap.ReadOnly = true;
+            this.colTenDangNhap.Width = 170;
+            // 
+            // colMatKhau
+            // 
+            this.colMatKhau.DataPropertyName = "matKhau";
+            this.colMatKhau.HeaderText = "Mật khẩu";
+            this.colMatKhau.MinimumWidth = 6;
+            this.colMatKhau.Name = "colMatKhau";
+            this.colMatKhau.ReadOnly = true;
+            this.colMatKhau.Visible = false;
+            this.colMatKhau.Width = 230;
+            // 
+            // colRemember
+            // 
+            this.colRemember.DataPropertyName = "remember";
+            this.colRemember.FalseValue = "False";
+            this.colRemember.HeaderText = "Nhớ mật khẩu";
+            this.colRemember.MinimumWidth = 6;
+            this.colRemember.Name = "colRemember";
+            this.colRemember.ReadOnly = true;
+            this.colRemember.TrueValue = "True";
+            this.colRemember.Width = 135;
+            // 
+            // colPhongBan
+            // 
+            this.colPhongBan.DataPropertyName = "phongBan";
+            this.colPhongBan.HeaderText = "Phòng Ban";
+            this.colPhongBan.MinimumWidth = 6;
+            this.colPhongBan.Name = "colPhongBan";
+            this.colPhongBan.ReadOnly = true;
+            this.colPhongBan.Width = 160;
+            // 
+            // frm_Account
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 564);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblAccount);
+            this.Name = "frm_Account";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frn_Account";
+            this.Load += new System.EventHandler(this.frm_Account_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.DataGridView dgvUserList;
+        private System.Windows.Forms.ToolStripLabel btnReload;
+        private System.Windows.Forms.ToolStripLabel btnExit;
+        private System.Windows.Forms.ToolStripLabel btnAdd;
+        private System.Windows.Forms.ToolStripLabel btnDelete;
+        private System.Windows.Forms.ToolStripLabel btnEdit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colHoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTenDangNhap;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMatKhau;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn colRemember;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPhongBan;
+    }
+}
---
+++ WindowsFormsQuaySo/frm_Account.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsQuaySo.Data;
+
+namespace WindowsFormsQuaySo
+{
+    public partial class frm_Account : Form
+    {
+        public frm_Account()
+        {
+            InitializeComponent();
+        }
+
+        UserDAO userDAO;
+        List<User> listUser;
+
+        private void frm_Account_Load(object sender, EventArgs e)
+        {
+            userDAO = new UserDAO();
+
+            GetDataToDataGridView();
+        }
+        private void GetDataToDataGridView()
+        {
+            //Lấy được data
+            userDAO.GetUser(Cls_Main.pathfile);
+            listUser = userDAO.listUser;
+
+            //Gán danh sách user vào data
+            BindingSource bindingSource = new BindingSource();
+            bindingSource.DataSource = listUser;
+            dgvUserList.DataSource = bindingSource;
+
+            //lblQuantity.Text = dgvUserList.RowCount.ToString();
+
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frm_Register frmRegister = new frm_Register();
+            frmRegister.isAdd = true;
+            frmRegister.ShowDialog();
+            GetDataToDataGridView();
+        }
+        User user;
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (user != null)
+            {
+                frm_Register frmRegister = new frm_Register();
+                frmRegister.isAdd = false;
+                frmRegister.user = user;
+                frmRegister.ShowDialog();
+                user = null;
+                GetDataToDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("Chọn tài khoản cần sửa");
+            }
+        }
+
+
+        private void dgvUserList_Click(object sender, EventArgs e)
+        {
+            if (dgvUserList.RowCount > 0)
+            {
+                user = new User()
+                {
+                    ID = Convert.ToInt32(dgvUserList.CurrentRow.Cells["colID"].Value.ToString()),
+                    HoTen = dgvUserList.CurrentRow.Cells["colHoTen"].Value.ToString(),
+                    TenDangNhap = dgvUserList.CurrentRow.Cells["colTenDangNhap"].Value.ToString(),
+                    MatKhau = dgvUserList.CurrentRow.Cells["colMatKhau"].Value.ToString(),
+                    Remember = Convert.ToBoolean(dgvUserList.CurrentRow.Cells["colRemember"].Value.ToString()),
+                    PhongBan = dgvUserList.CurrentRow.Cells["colPhongBan"].Value.ToString()
+
+                };
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (user != null)
+            {
+                userDAO.listUser.Remove(user);
+                userDAO.WriteUser(Cls_Main.pathfile);
+                user = null;
+                GetDataToDataGridView();
+            }
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            GetDataToDataGridView();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
