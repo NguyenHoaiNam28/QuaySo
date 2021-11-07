ba0af14e22d3ad7b015ca8c8f2c309bc96b36694
Ngo Van Duc <mlvhliu@gmail.com>
2021-11-07 15:40:44 +0700

    Them from Register

---
+++ WindowsFormsQuaySo/frm_Register.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace WindowsFormsQuaySo
+{
+    partial class frm_Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txthoTen = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkbRemember = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboPhongBan = new System.Windows.Forms.ComboBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(535, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng ký tài khoản";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 23);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Tên đăng nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 23);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(212, 228);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(278, 22);
+            this.txtMatKhau.TabIndex = 29;
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(212, 175);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(278, 22);
+            this.txtTenDangNhap.TabIndex = 28;
+            // 
+            // btnExit
+            // 
+            this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnExit.BackColor = System.Drawing.SystemColors.Control;
+            this.btnExit.Location = new System.Drawing.Point(385, 368);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(105, 41);
+            this.btnExit.TabIndex = 31;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txthoTen
+            // 
+            this.txthoTen.Location = new System.Drawing.Point(212, 121);
+            this.txthoTen.Name = "txthoTen";
+            this.txthoTen.Size = new System.Drawing.Size(278, 22);
+            this.txthoTen.TabIndex = 33;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 23);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Tên hiển thị";
+            // 
+            // chkbRemember
+            // 
+            this.chkbRemember.AutoSize = true;
+            this.chkbRemember.Location = new System.Drawing.Point(212, 272);
+            this.chkbRemember.Name = "chkbRemember";
+            this.chkbRemember.Size = new System.Drawing.Size(56, 21);
+            this.chkbRemember.TabIndex = 34;
+            this.chkbRemember.Text = "Nhớ";
+            this.chkbRemember.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(33, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 23);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Phòng Ban";
+            // 
+            // cboPhongBan
+            // 
+            this.cboPhongBan.FormattingEnabled = true;
+            this.cboPhongBan.Location = new System.Drawing.Point(212, 317);
+            this.cboPhongBan.Name = "cboPhongBan";
+            this.cboPhongBan.Size = new System.Drawing.Size(278, 24);
+            this.cboPhongBan.TabIndex = 36;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(212, 70);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(278, 22);
+            this.txtID.TabIndex = 38;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(33, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 23);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "ID";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.Control;
+            this.btnUpdate.Location = new System.Drawing.Point(236, 368);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(105, 41);
+            this.btnUpdate.TabIndex = 39;
+            this.btnUpdate.Text = "Cập nhập";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click_1);
+            // 
+            // frm_Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 434);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cboPhongBan);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chkbRemember);
+            this.Controls.Add(this.txthoTen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_Register";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_Register";
+            this.Load += new System.EventHandler(this.frm_Register_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txthoTen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkbRemember;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboPhongBan;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
---
+++ WindowsFormsQuaySo/frm_Register.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsQuaySo.Data;
+
+namespace WindowsFormsQuaySo
+{
+    public partial class frm_Register : Form
+    {
+        public frm_Register()
+        {
+            InitializeComponent();
+        }
+        public bool isAdd = false;
+         UserDAO userDAO;
+        private void frm_Register_Load(object sender, EventArgs e)
+        {
+            userDAO = new UserDAO();
+            userDAO.GetUser(Cls_Main.pathfile);
+            LoadDataToComPhongBan();
+            if (isAdd == true)
+            {
+                //Thêm User
+                txtID.Text = CreateMaxID();
+                txtID.Enabled = false;
+
+
+            }
+            else
+            {
+                //Sửa user
+                SetDataToControl(user);
+            }
+        }
+
+        private void LoadDataToComPhongBan()
+        {
+            List<PhongBan> userType = new List<PhongBan>() {
+                new PhongBan(){IDPhongBan = 1, NamePhongban = "Phòng hành chính"},
+                new PhongBan(){IDPhongBan = 2, NamePhongban = "Phòng IT"},
+                new PhongBan(){IDPhongBan = 3, NamePhongban = "Phòng kế toán"},
+                new PhongBan(){IDPhongBan = 4, NamePhongban = "Phòng nội vụ"}
+            };
+
+
+            //Gán vào Datasource cua combo
+            cboPhongBan.DataSource = userType;
+            //Thiết lập thuộc tính
+            cboPhongBan.DisplayMember = "NamePhongban";
+            cboPhongBan.ValueMember = "IDPhongBan";
+        }
+
+        public User user;
+
+        private void SetDataToControl(User user)
+        {
+            txtID.Text = user.ID.ToString();
+            txthoTen.Text = user.HoTen.ToString();
+            txtTenDangNhap.Text = user.TenDangNhap.ToString();
+            txtMatKhau.Text = user.MatKhau.ToString();
+            chkbRemember.Checked = user.Remember;
+            cboPhongBan.SelectedValue = user.PhongBan;
+        }
+
+        string err = string.Empty;
+
+
+        private string CreateMaxID()
+        {
+            string maxID = string.Empty;
+            maxID = userDAO.GetMaxID().ToString();
+            return maxID;
+        }
+        
+
+        private void btnUpdate_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txthoTen.Text))
+            {
+                if (!string.IsNullOrEmpty(txtTenDangNhap.Text))
+                {
+                    if (!string.IsNullOrEmpty(txtMatKhau.Text))
+                    {
+                        if (cboPhongBan.SelectedIndex > -1)
+                        {
+                            user = new User()
+                            {
+                                ID = Convert.ToInt32(txtID.Text),
+                                HoTen = txthoTen.Text,
+                                TenDangNhap = txtTenDangNhap.Text,
+                                MatKhau = txtMatKhau.Text,
+                                Remember = chkbRemember.Checked,
+                                PhongBan = cboPhongBan.Text
+                            };
+
+                            if (UpdateUser(ref err, user))
+                            {
+                                MessageBox.Show("Tạo thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                this.Close();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Hãy chọn phòng ban", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Hãy mật khẩu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Hãy nhập tên đăng nhập", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Hãy nhập tên hiển thị", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private bool UpdateUser(ref string err, User user)
+        {
+            try
+            {
+                if (isAdd)
+                {
+                    userDAO.listUser.Add(user);
+                    userDAO.WriteUser(Cls_Main.pathfile);
+                    return true;
+                }
+                else
+                {
+                    foreach (User item in userDAO.listUser)
+                    {
+                        if (item.ID == user.ID)
+                        {
+                            item.HoTen = user.HoTen;
+                            item.TenDangNhap = user.TenDangNhap;
+                            item.MatKhau = user.MatKhau;
+                            item.Remember = user.Remember;
+                            item.PhongBan = user.PhongBan;
+                        }
+                    }
+                    userDAO.WriteUser(Cls_Main.pathfile);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                err = e.Message;
+            }
+            return false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
