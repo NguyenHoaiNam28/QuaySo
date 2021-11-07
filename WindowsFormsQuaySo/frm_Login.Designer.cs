024cff9d9b029cae7e3587a5d91e698867d22e86
Nguyễn Hoài Nam <namqkth@gmail.com>
2021-11-07 15:27:38 +0700

    Them from Login

---
+++ WindowsFormsQuaySo/frm_Login.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace WindowsFormsQuaySo
+{
+    partial class frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chkbRemember = new System.Windows.Forms.CheckBox();
+            this.txbPassWord = new System.Windows.Forms.TextBox();
+            this.txbUserName = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(41, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chkbRemember
+            // 
+            this.chkbRemember.AutoSize = true;
+            this.chkbRemember.Location = new System.Drawing.Point(232, 147);
+            this.chkbRemember.Name = "chkbRemember";
+            this.chkbRemember.Size = new System.Drawing.Size(56, 21);
+            this.chkbRemember.TabIndex = 28;
+            this.chkbRemember.Text = "Nhớ";
+            this.chkbRemember.UseVisualStyleBackColor = true;
+            // 
+            // txbPassWord
+            // 
+            this.txbPassWord.Location = new System.Drawing.Point(217, 115);
+            this.txbPassWord.Name = "txbPassWord";
+            this.txbPassWord.Size = new System.Drawing.Size(278, 22);
+            this.txbPassWord.TabIndex = 27;
+            // 
+            // txbUserName
+            // 
+            this.txbUserName.Location = new System.Drawing.Point(217, 68);
+            this.txbUserName.Name = "txbUserName";
+            this.txbUserName.Size = new System.Drawing.Size(278, 22);
+            this.txbUserName.TabIndex = 26;
+            this.txbUserName.Leave += new System.EventHandler(this.txbUserName_Leave_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnExit.BackColor = System.Drawing.SystemColors.Control;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Highlight;
+            this.btnExit.Location = new System.Drawing.Point(362, 185);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(105, 41);
+            this.btnExit.TabIndex = 30;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Highlight;
+            this.btnLogin.Location = new System.Drawing.Point(230, 185);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(105, 41);
+            this.btnLogin.TabIndex = 29;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 23);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 23);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Tên đăng nhập";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(539, 42);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Đăng nhập";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 285);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chkbRemember);
+            this.Controls.Add(this.txbPassWord);
+            this.Controls.Add(this.txbUserName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_Login";
+            this.Load += new System.EventHandler(this.frm_Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox chkbRemember;
+        private System.Windows.Forms.TextBox txbPassWord;
+        private System.Windows.Forms.TextBox txbUserName;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ WindowsFormsQuaySo/frm_Login.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsQuaySo.Data;
+
+namespace WindowsFormsQuaySo
+{
+    public partial class frm_Login : Form
+    {
+        public frm_Login()
+        {
+            InitializeComponent();
+        }
+        private List<User> listUser;
+        UserDAO _userDAO;
+
+        private void frm_Login_Load(object sender, EventArgs e)
+        {
+            _userDAO = new UserDAO();
+            _userDAO.GetUser(Cls_Main.pathfile);
+            Cls_Main._listUser = _userDAO.listUser;
+
+            listUser = Cls_Main._listUser;
+        }
+        private void btnLogin_Click_1(object sender, EventArgs e)
+        {
+            // Kiểm tra đăng nhập
+            if (!string.IsNullOrEmpty(txbUserName.Text))
+            {
+                if (!string.IsNullOrEmpty(txbPassWord.Text))
+                {
+
+                    if (Kiemtradangnhap(txbUserName.Text, txbPassWord.Text))
+                    {
+
+                        frm_QuaySoMayMan fQuaySoMayMan = new frm_QuaySoMayMan();
+                        this.Hide();
+                        fQuaySoMayMan.ShowDialog();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nhập sai tên đăng nhập hoặc mật khẩu", "Thông báo");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nhập mật khẩu", "Thông báo");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nhập tên đăng nhập", "Thông báo");
+            }
+        }
+        private bool Kiemtradangnhap(string tenDangNhap, string matKhau)
+        {
+            foreach (User item in listUser)
+            {
+                if (item.TenDangNhap.Equals(tenDangNhap) && item.MatKhau.Equals(matKhau))
+                {
+                    item.Remember = chkbRemember.Checked;
+                    Cls_Main._staticUser = item;
+                    return true;
+                }
+
+            }
+            return false;
+
+        }
+
+        private void txbUserName_Leave_1(object sender, EventArgs e)
+        {
+            foreach (User item in listUser)
+            {
+                if (item.TenDangNhap == txbUserName.Text && item.Remember)
+                {
+                    chkbRemember.Checked = true;
+                    txbPassWord.Text = item.MatKhau;
+                    btnLogin.Focus();
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
